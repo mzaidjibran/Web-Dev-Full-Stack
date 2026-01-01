cb4bb6335fbf52fca7533fdca41d58e62be67ddc
mzaidjibran <mzaidjibran07@gmail.com>
2026-01-01 17:53:55 +0500

    If-Else statemnet

---
+++ JS Conditions/index.js
@@ -0,0 +1,10 @@
+console.log("Hello I am conditional statement!");
+
+let age = 11;
+let age2 = 2;
+if ((age + age2) > 18) {
+    console.log("You are eligible to drive");
+}
+else {
+    console.log("You are not eligible to drive");
+}
