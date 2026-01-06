ba469189fca1e56269d7d4c1c4f2d7d684fcb786
mzaidjibran <mzaidjibran07@gmail.com>
2026-01-06 18:36:03 +0500

    Functions in JS

---
+++ JS Functions/index.js
@@ -0,0 +1,17 @@
+function test(name) {
+    console.log("My name is " + name);
+    console.log(name + " is very smart.");
+    console.log("Why you beat " + name);
+    console.log("It feels good to meet " + name + " due to his attiquates.");
+}
+
+test("Zaid")
+
+test("Ali")
+
+function sum(a, b) {
+    return a + b;
+}
+
+result = sum(76, 89);
+console.log("The sum of two numbers is " + result);
