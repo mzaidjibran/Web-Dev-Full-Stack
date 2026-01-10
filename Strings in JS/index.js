dd4b78dd34aa0b7117e665f5c6ed3864efa3a3ce
mzaidjibran <mzaidjibran07@gmail.com>
2026-01-10 12:22:08 +0500

    Strings

---
+++ Strings in JS/index.js
@@ -0,0 +1,16 @@
+let a = "Hello, my name is Zaid Jibran";
+console.log(a);
+console.log(a.length);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+console.log(a.slice(0, 5));
+console.log(a.slice(7, 9));
+
+console.log(a.replace("Hello", "Hi"));
