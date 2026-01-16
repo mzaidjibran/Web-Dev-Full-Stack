5928c8bf959b298e3497de10694a7520ea24aa39
mzaidjibran <mzaidjibran07@gmail.com>
2026-01-16 18:46:54 +0500

    DOM

---
+++ DOM in JS/index.js
@@ -0,0 +1,15 @@
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.firstElementChild);
+
+console.log(document.body.firstElementChild.children);
+
+console.log(document.body.firstElementChild.children[2].previousElementSibling);
+
+// console.log(document.body.lastElementChild);
+
+// console.log(document.body.lastElementChild.parentElement);
+
+
